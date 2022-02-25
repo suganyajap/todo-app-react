4259e1f6bc6f452d8ff6adec62211e3a48a8b160
SUNDARAVADIVU R <suganyajap@gmail.com>
2022-02-25 07:13:52 +0530

    addtask changed

--- src/Component/AddTask.js
+++ src/Component/AddTask.js
@@ -1,6 +1,9 @@
 import './AddTask.css'
 import React ,{useState}from 'react'
-import axios from 'axios'
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 function AddTask(props) {
     const [tasks,setTasks] = useState("")
     const addtask = () => {
@@ -18,8 +21,12 @@ function AddTask(props) {
     }
     return (
         <div className = 'addtask'>
-            <input type='text' placeholder = 'Add Task . . .' value = {tasks} onChange = {event => setTasks(event.target.value)}/>
-            <button onClick = {() => addtask()}>Add Task</button>
+            {/* <input type='text' placeholder = 'Add Task . . .' value = {tasks} onChange = {event => setTasks(event.target.value)}/>
+            <button onClick = {() => addtask()}>Add Task</button> */}
+            <TextField id="standard-basic" label="addtask" variant="standard" type='text' placeholder = 'Add Task . . .' value = {tasks} onChange = {event => setTasks(event.target.value)} />
+            <Button variant="contained" color="success" onClick = {() => addtask()}>
+  Add Task</Button>
+
         </div>
     )
 }
